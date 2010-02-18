05defa9a339373227e6b8fa02199c453cf2a51f9
Chris Keitel <chris@3-byte.com>
2010-02-18 21:29:50 +0000

    Add a new appender to support capturing recently logged events for debugging purposes.  For use in Paley Center apps.

---
+++ Logging/LogUtil.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Deployment.Application;
+using System.Net;
+using log4net;
+using log4net.Appender;
+using log4net.Repository.Hierarchy;
+
+namespace ThreeByte.Logging
+{
+    public static class LogUtil
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LogUtil));
+
+        /// <summary>
+        /// For reporting purposes, gather and report useful information about the execution context
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDiagnosticString() {
+            string context = string.Format("Timestamp: {0}\n", DateTime.Now);
+
+            try {
+                context += string.Format("Hostname: {0}\n", System.Environment.MachineName);
+                context += string.Format("IPAddress: {0}\n", GetIPLocalAddress());
+                context += string.Format("Username: {0}\n", System.Environment.UserName);
+                if(ApplicationDeployment.IsNetworkDeployed) {
+                    context += string.Format("Application Version Number: {0}\n", ApplicationDeployment.CurrentDeployment.CurrentVersion);
+                } else {
+                    context += "Application Version: Desktop/Debug\n";
+                }
+            } catch(Exception ex) {
+                log.Error("Cannot create diagnostic string", ex);
+                context += "Exception: " + ex.Message;
+            }
+            return context;
+        }
+
+        public static string GetIPLocalAddress() {
+
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            string ipAddress = string.Empty;
+            foreach(IPAddress ip in hostEntry.AddressList) {
+                ipAddress += ip.ToString() + "; ";
+            }
+            return ipAddress;
+        }
+
+        public static string GetRecentEventString(){
+            string eventString = string.Empty;
+            try {
+                Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+                foreach(IAppender app in hierarchy.Root.Appenders) {
+                    if(app is RecentEventAppender) {
+                        eventString += app.Name + ": " + ((RecentEventAppender)app).GetRecentEventString();
+                    }
+                }    
+            } catch(Exception ex) {
+                eventString += string.Format("Cannot create recent event string: {0}", ex.Message);
+            }
+            return eventString;
+
+        }
+
+    }
+}
---
+++ Logging/RecentEventAppender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using log4net.Appender;
+using log4net.Core;
+
+
+namespace ThreeByte.Logging
+{
+    public class RecentEventAppender : AppenderSkeleton
+    {
+        Queue<LoggingEvent> _eventQueue;
+
+        public int RecentEventLimit { get; set; }
+
+        public RecentEventAppender() {
+            RecentEventLimit = 30;  //Defaults to a reasonable number of events
+            _eventQueue = new Queue<LoggingEvent>(RecentEventLimit);
+        }
+
+        protected override void Append(LoggingEvent loggingEvent) {
+
+            lock(_eventQueue) {
+                _eventQueue.Enqueue(loggingEvent);
+                while(_eventQueue.Count > RecentEventLimit) {
+                    _eventQueue.Dequeue();  //These get abandoned
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the currently queued list of recent events.  This is synchronized with the Append method, so this will
+        /// hold up the logging of other events.  This method is not expected to be called frequently. 
+        /// </summary>
+        /// <returns></returns>
+        public List<LoggingEvent> GetRecentEvents() {
+            lock(_eventQueue) {
+                return _eventQueue.ToList();  //Must lock during implicit enumeration
+            }
+        }
+
+        public string GetRecentEventString() {
+            List<LoggingEvent> recentEvents = GetRecentEvents();
+            string logString = string.Empty;
+            foreach(LoggingEvent loggedEvent in recentEvents) {
+                logString += string.Format("{0}\n", loggedEvent.RenderedMessage);
+            }
+            return logString;
+        }
+
+    }
+}
