6da8e92fcdfc5917a82957410f6286b1c7e7f342
Chris Keitel <chris@3-byte.com>
2010-06-01 22:25:17 +0000

    Added some logic for Paley Center use.

---
+++ Converters/UppercaseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ThreeByte.Converters
+{
+    public class UppercaseConverter : IValueConverter
+    {
+        //If these are strings ro
+        public object Convert(object value, Type tagerType, object parameter, CultureInfo culture) {
+            return ((value == null) ? null : value.ToString().ToUpper());
+        }
+
+        public object ConvertBack(object value, Type tagerType, object parameter, CultureInfo culture) {
+            throw new InvalidOperationException("Cannot convert back from UppercaseConverter!");
+        }
+        
+
+    }
+}
