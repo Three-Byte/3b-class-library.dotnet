ee5306f943fb6f2b1c2bdb5a01330b8d47d178e2
Chris Keitel <chris@3-byte.com>
2011-04-29 13:29:15 +0000

    Added library converter.

---
+++ Converters/IsNullConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Data.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows;
+
+namespace ThreeByte.Converters
+{
+    public class IsNullConverter : IValueConverter
+    {
+        public bool Opposite { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if(Opposite) {
+                return !(value == null);
+            } else {
+                return (value == null);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
