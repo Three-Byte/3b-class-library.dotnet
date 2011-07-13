eb8df6292dd92bde0f49548610b81ecd4c207f4c
Chris Keitel <chris@3-byte.com>
2011-07-13 21:26:05 +0000

    Added a FrozenImageConverter which prevents FileAccess errors when WPF is displaying graphic images.  refs #6378

---
+++ Converters/FrozenImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Data.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+
+namespace ThreeByte.Converters
+{
+    public class FrozenImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            //Take a URI and 
+
+            string path = System.Convert.ToString(value);
+            if(path == null) {
+                return null;
+            }
+
+            BitmapImage newBitmap = new BitmapImage();
+            newBitmap.BeginInit();
+            newBitmap.CacheOption = BitmapCacheOption.OnLoad;
+            using(FileStream fs = new FileStream(path, FileMode.Open)) {
+                newBitmap.StreamSource = fs;
+                newBitmap.EndInit();
+                newBitmap.Freeze();
+            }
+            return newBitmap;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
