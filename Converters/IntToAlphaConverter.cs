099d26e7c7bd11f81b17d958ec45dff13f2ce218
Chris Keitel <chris@3-byte.com>
2011-08-22 21:52:09 +0000

    Added some utility converters   refs #6725

---
+++ Converters/IntToAlphaConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ThreeByte.Converters
+{
+    public class IntToAlphaConverter : IValueConverter
+    {
+        public char _baseChar = 'A';
+        public char BaseChar {
+            get {
+                return _baseChar;
+            }
+            set {
+                _baseChar = value;
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            int intVal = System.Convert.ToInt32(value);
+            return string.Format("{0}", (char)(BaseChar + intVal));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new Exception("Cannot convert back from IntToAlphaConverter");
+        }
+    }
+   
+}
---
+++ Converters/IsNullOrWhitespaceConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Data.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows;
+
+namespace ThreeByte.Converters
+{
+    public class IsNullOrWhitespaceConverter : IValueConverter
+    {
+        public bool Opposite { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if(Opposite) {
+                return !string.IsNullOrWhiteSpace(value as string);
+            } else {
+                return string.IsNullOrWhiteSpace(value as string);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
