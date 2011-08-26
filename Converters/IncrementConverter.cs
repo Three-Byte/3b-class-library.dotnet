5dcda7a473cc238bac1cea7c2e709d8af04ff49f
Chris Keitel <chris@3-byte.com>
2011-08-26 20:14:52 +0000

    Checkin increment converter so the AddIn will build.  refs #6726

---
+++ Converters/IncrementConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Data.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows;
+
+namespace ThreeByte.Converters
+{
+    public class IncrementConverter : IValueConverter
+    {
+        public int Increment { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return System.Convert.ToInt32(value) + Increment;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return System.Convert.ToInt32(value) - Increment;
+        }
+    }
+}
