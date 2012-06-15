687f298eaf94e04198e622266ad2e91f175d3c0a
Chris Keitel <chris@3-byte.com>
2012-06-15 16:56:42 +0000

    Added IntToVisibilityConverter.cs  refs #8083

---
+++ Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ThreeByte.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+
+        public int IntValue { get; set; }
+        public Visibility DefaultVisibility { get; set; }
+        public Visibility HighlightVisibility { get; set; }
+
+        public IntToVisibilityConverter() {
+            IntValue = 0;
+            DefaultVisibility = Visibility.Visible;
+            HighlightVisibility = Visibility.Hidden;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            int intVal = System.Convert.ToInt32(value);
+            return (intVal == IntValue ? HighlightVisibility : DefaultVisibility);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new Exception("Cannot convert back from IntToVisibilityConverter");
+        }
+    }
+   
+}
