b2eded78ee78c812ffd1e7b3cf5c0b4e0367b978
Chris Keitel <chris@3-byte.com>
2012-06-24 19:54:28 +0000

    Added DMX animation to the library.

---
+++ DMX/DmxAnimation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace ThreeByte.DMX
+{
+    public class DmxAnimation : DmxAnimationBase
+    {
+
+        protected override Freezable CreateInstanceCore() {
+            return new DmxAnimation();
+        }
+
+        public static readonly DependencyProperty FromProperty = DependencyProperty.Register("From", typeof(Dictionary<int, int>), typeof(DmxAnimation));
+
+        public Dictionary<int, int> From {
+            get { return (Dictionary<int, int>)GetValue(FromProperty); }
+            set { SetValue(FromProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToProperty = DependencyProperty.Register("To", typeof(Dictionary<int, int>), typeof(DmxAnimation));
+
+        public Dictionary<int, int> To {
+            get { return (Dictionary<int, int>)GetValue(ToProperty); }
+            set { SetValue(ToProperty, value); }
+        }
+
+
+        //Allocated once to prevent memory allocation on each calculation
+        Dictionary<int, int> returnValue1 = new Dictionary<int, int>();
+        Dictionary<int, int> returnValue2 = new Dictionary<int, int>();
+        bool flip = false;
+
+        private Dictionary<int, int> fromValues;
+        private Dictionary<int, int> toValues;
+
+        protected override Dictionary<int, int> GetCurrentValueCore(Dictionary<int, int> defaultOriginValue,
+                                                                    Dictionary<int, int> defaultDestinationValue,
+                                                                    AnimationClock animationClock) {
+
+            if(animationClock.CurrentProgress == null){
+                return null;
+            }
+
+            double progress = animationClock.CurrentProgress.Value;
+            Dictionary<int, int> returnValue = flip ? returnValue1 : returnValue2;
+            flip = !flip;
+            returnValue.Clear();
+
+            //Choose the correct From/To values
+            fromValues = From ?? defaultOriginValue;
+            toValues = To ?? defaultDestinationValue;
+
+            //Interpolate the values
+            foreach(int c in fromValues.Keys) {
+                int newValue = fromValues[c];
+                if(toValues.ContainsKey(c)) {
+                    //Basic linear interpolation
+                    newValue = (int)Math.Round(((1.0 - progress) * fromValues[c]) + (progress * toValues[c]));
+                } else {
+                    //This value doesn't change
+                }
+                returnValue[c] = newValue;
+            }
+
+            return returnValue;
+        }
+
+        public override bool IsDestinationDefault {
+            get {
+                //TODO: What should this be?
+                return base.IsDestinationDefault;
+            }
+        }
+    }
+}
---
+++ DMX/DmxAnimationBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Animation;
+
+namespace ThreeByte.DMX
+{
+    public abstract class DmxAnimationBase : AnimationTimeline
+    {
+
+        public override Type TargetPropertyType {
+            get { return typeof(Dictionary<int, int>); }
+        }
+
+        public sealed override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock) {
+            //Validate default origin and destination types
+            return GetCurrentValueCore(defaultOriginValue as Dictionary<int, int>, defaultDestinationValue as Dictionary<int, int>, animationClock);
+        }
+
+        protected abstract Dictionary<int, int> GetCurrentValueCore(Dictionary<int, int> defaultOriginValue, Dictionary<int, int> defaultDestinationValue, AnimationClock animationClock);
+
+    }
+}
