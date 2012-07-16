f6f46d5e37bb9d5b9fa914987d8237388484dbd0
Mike <mike@3-byte.com>
2012-07-16 21:26:05 +0000

    Created TimeSpanToDateTimeConverter.cs.  refs #8320

---
+++ Converters/TimeSpanToDateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ThreeByte.Converters {
+    public class TimeSpanToDateTimeConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            TimeSpan timespan = (TimeSpan)value;
+            DateTime datetime = new DateTime(timespan.Ticks);
+            return datetime;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            DateTime datetime = (DateTime)value;
+            TimeSpan timespan = TimeSpan.FromTicks(datetime.Ticks);
+            return timespan;
+        }
+    }
+}
