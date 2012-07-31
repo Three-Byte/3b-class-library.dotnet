df1b864cbcfbc37eb44ec35e9b655852c3bd9595
Mike <mike@3-byte.com>
2012-07-31 20:54:45 +0000

    Added TimeSpanToStringConverter.cs in order to handle formatting of negative TimeSpans.  refs #8388

---
+++ Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ThreeByte.Converters {
+    public class TimeSpanToStringConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            TimeSpan time = (TimeSpan)value;
+
+            string neg = @"\ ";
+            if(time < TimeSpan.Zero) {
+                neg = @"\-";
+            }
+
+            return time.ToString(neg + @"hh\:mm\:ss\.f");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
