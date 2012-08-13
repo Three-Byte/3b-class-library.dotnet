bb1fb6a61f0de120c24b4a40716efe0e4ceab72b
Chris Keitel <chris@3-byte.com>
2012-08-13 18:34:44 +0000

    Added two new converters for MoMath.

---
+++ Converters/ParameterEqualVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ThreeByte.Converters
+{
+    public class ParameterEqualVisibilityConverter : IValueConverter
+    {
+        public Visibility DefaultVisibility { get; set; }
+        public Visibility HighlightVisibility { get; set; }
+
+        public ParameterEqualVisibilityConverter() {
+            DefaultVisibility = Visibility.Visible;
+            HighlightVisibility = Visibility.Hidden;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            return (value == parameter ? HighlightVisibility : DefaultVisibility);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new Exception("Cannot convert back from ParameterEqualVisibilityConverter");
+        }
+    }
+   
+}
---
+++ Converters/StringToCharArrayConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Data.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ThreeByte.Converters
+{
+    public class StringToCharArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            string s = value as string;
+            if(string.IsNullOrWhiteSpace(s)) {
+                return new char[0];
+            }
+            return s.ToCharArray();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            char[] chars = value as char[];
+            if(chars == null) {
+                return string.Empty;
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in chars) {
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
