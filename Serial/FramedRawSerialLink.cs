ad29bc754fb8cc8a910b5da1d91a954789cac275
Chris Keitel <chris@3-byte.com>
2012-12-14 18:51:09 +0000

    Added a new framed raw serial link.

---
+++ Serial/FramedRawSerialLink.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.ComponentModel;
+using System.Threading;
+using System.IO;
+
+using log4net;
+
+
+namespace ThreeByte.Serial {
+    public class FramedRawSerialLink : IDisposable, INotifyPropertyChanged {
+        private readonly ILog log = LogManager.GetLogger(typeof(FramedRawSerialLink));
+
+        //Observable Interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        private const int MAX_DATA_SIZE = 100;
+
+        private SerialLink _serialLink;
+
+        public SerialFrame SendFrame { get; set; }
+        public SerialFrame ReceiveFrame { get; set; }
+
+        public bool HasData {
+            get {
+                return (_incomingData.Count > 0);
+            }
+        }
+
+        #region SerialLink Pass-Through Properties
+        public string COMPort {
+            get { return _serialLink.COMPort; }
+            set { _serialLink.COMPort = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether messages should be propogated to the network or not
+        /// </summary>
+        public bool Enabled {
+            get { return _serialLink.Enabled; }
+            set {
+                _serialLink.Enabled = value;
+                if(!value) {
+                    //If the link is disabled, clear the buffer of messages
+                    lock(_incomingData) {
+                        _incomingData.Clear();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not there is current network activity with this node
+        /// </summary>
+        public bool IsOpen {
+            get { return _serialLink.IsOpen; }
+        }
+
+        /// <summary>
+        /// Gets a representation of the last Exception that was thrown by the underlying connection
+        /// </summary>
+        public Exception Error {
+            get { return _serialLink.Error; }
+        }
+        #endregion //AsyncNetworkLink Pass-Through Properties
+
+
+        public event EventHandler DataReceived;
+
+        private List<byte[]> _incomingData;
+        private MemoryStream _incomingBuffer;
+
+
+        public FramedRawSerialLink(string comPort, bool enabled = true, int baudRate = 9600) {
+
+            _incomingBuffer = new MemoryStream(2048);
+            _incomingData = new List<byte[]>();
+
+			_serialLink = new SerialLink(comPort, enabled, baudRate, 7, System.IO.Ports.Parity.Even);
+            _serialLink.DataReceived += new EventHandler(_serialLink_DataReceived);
+            _serialLink.PropertyChanged += new PropertyChangedEventHandler(_serialLink_PropertyChanged);
+
+        }
+
+        #region Implements IDisposable
+        private bool _disposed = false;
+        /// <summary>
+        /// Implementation of IDisposable interface.  Cancels the thread and releases resources.
+        /// Clients of this class are responsible for calling it.
+        /// </summary>
+        public void Dispose() {
+            if(_disposed) {
+                return;  //Dispose has already been called
+            }
+            _disposed = true;
+            log.Info("Cleaning up serial resources");
+            _serialLink.Dispose();
+        }
+        #endregion //Implements IDisposable
+
+        void _serialLink_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+            if(e.PropertyName == "Enabled"
+                || e.PropertyName == "IsConnected"
+                || e.PropertyName == "Error"
+                || e.PropertyName == "COMPort") {
+                NotifyPropertyChanged(e.PropertyName);
+            }
+        }
+
+        private int _headerPos = 0;
+        private int _footerPos = 0;
+
+        void _serialLink_DataReceived(object sender, EventArgs e) {
+            bool hasNewData = false;
+
+            byte[] header = new byte[0];
+            if(ReceiveFrame != null && ReceiveFrame.Header != null) {
+                header = ReceiveFrame.Header;
+            }
+
+            byte[] footer = new byte[0];
+            if(ReceiveFrame != null && ReceiveFrame.Footer != null) {
+                footer = ReceiveFrame.Footer;
+            }
+
+            while(_serialLink.HasData) {
+                lock(_incomingBuffer) {
+                    byte[] buffer = _serialLink.GetMessage();
+
+                    //Must validate this buffer - see issue #4934
+                    if(buffer == null) {
+                        break;
+                    }
+                    for(int i = 0; i < buffer.Length; i++) {
+                        if(_headerPos < header.Length - 1 && buffer[i] == header[_headerPos]) {
+                            _headerPos++;
+                        } else if(_headerPos == header.Length - 1 && buffer[i] == header[_headerPos]) {
+                            _headerPos = 0;
+                            _footerPos = 0;
+                            _incomingBuffer.Position = 0; //Reset to the beginning
+                        } else if(_footerPos < footer.Length - 1 && buffer[i] == footer[_footerPos]) {
+                            _footerPos++;
+                        } else if(_footerPos == footer.Length - 1 && buffer[i] == footer[_footerPos]) {
+                            _footerPos = 0;  //Reset Footer
+
+                            byte[] frameData = new byte[_incomingBuffer.Position];
+                            Array.Copy(_incomingBuffer.GetBuffer(), frameData, frameData.Length);
+
+                            lock(_incomingData) {
+                                _incomingData.Add(frameData);
+                                if(_incomingData.Count > MAX_DATA_SIZE) {
+                                    //Purge messages from the end of the list to prevent overflow
+                                    log.Error("Too many incoming messages to handle: " + _incomingData.Count);
+                                    _incomingData.RemoveAt(_incomingData.Count - 1);
+                                }
+                            }
+                            hasNewData = true;
+                            _incomingBuffer.Position = 0;
+                        } else {
+                            _headerPos = 0;
+                            _incomingBuffer.WriteByte(buffer[i]);
+                        }
+                    }
+                }
+
+            }
+
+            if(hasNewData && DataReceived != null && !_disposed) {
+                DataReceived(this, new EventArgs());
+            }
+
+        }
+
+        /// <summary>
+        /// Asynchronously sends the tcp message, waiting until the connection is reestablihsed if necessary
+        /// </summary>
+        /// <param name="message"></param>
+        public void SendData(byte[] message) {
+            if(_disposed) {
+                throw new ObjectDisposedException("Cannot send message on disposed FramedRawSerialLink");
+            }
+
+            //Don't do anything if the link is not enabled
+            if(!Enabled) return;
+
+            //Add the header and footer
+            byte[] header = new byte[0];
+            if(SendFrame != null && SendFrame.Header != null) {
+                header = SendFrame.Header;
+            }
+            byte[] footer = new byte[0];
+            if(SendFrame != null && SendFrame.Footer != null) {
+                footer = SendFrame.Footer;
+            }
+            byte[] messageBytes = new byte[message.Length + header.Length + footer.Length];
+
+            header.CopyTo(messageBytes, 0);
+            message.CopyTo(messageBytes, header.Length);
+            footer.CopyTo(messageBytes, message.Length + header.Length);
+
+            //log.Info("Debug: " + string.Format("{0} {1} {2}\r", message.Length, header.Length, messageBytes.ToString()));
+
+            if(_serialLink != null)
+                try {
+                    _serialLink.SendData(messageBytes);
+                } catch(ObjectDisposedException ode) {
+                    log.Error(ode.Message);
+                } catch(Exception ex) {
+                    //Also possible for the serial link to raise and UnauthorizedAccessException here
+                    log.Error("SendMessage error", ex);
+                }
+        }
+
+        /// <summary>
+        /// Fetches and removes (pops) the next available message as received on this link in order (FIFO)
+        /// </summary>
+        /// <returns>null if the link is not Enabled or there are no messages currently queued to return, a string otherwise.</returns>
+        public byte[] GetData() {
+            if(_disposed) {
+                throw new ObjectDisposedException("Cannot get message from disposed FramedRawSerialLink");
+            }
+
+            //Return null if the link is not enabled
+            if(!Enabled) return null;
+
+            byte[] newMessage = null;
+            lock(_incomingData) {
+                if(HasData) {
+                    newMessage = _incomingData[0];
+                    //throw new InvalidOperationException("Cannot return any data [" + newMessage + "]");
+                    _incomingData.RemoveAt(0);
+                }
+            }
+            return newMessage;
+        }
+
+    }
+}
