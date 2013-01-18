e06f00dce4f20b794d67f97b6616260595cbd0ff
Amichai Levy <amichai@3-byte.com>
2013-01-18 18:09:21 +0000

    Added support for TemporalOffset to the DMX animation. refs #10961

--- DMX/DmxAnimation.cs
+++ DMX/DmxAnimation.cs
@@ -5,11 +5,8 @@ using System.Text;
 using System.Windows;
 using System.Windows.Media.Animation;
 
-namespace ThreeByte.DMX
-{
-    public class DmxAnimation : DmxAnimationBase
-    {
-
+namespace ThreeByte.DMX {
+    public class DmxAnimation : DmxAnimationBase {
         protected override Freezable CreateInstanceCore() {
             return new DmxAnimation();
         }
@@ -34,15 +31,129 @@ namespace ThreeByte.DMX
         Dictionary<int, int> returnValue2 = new Dictionary<int, int>();
         bool flip = false;
 
+        ///For every channel key, we get the amount of channels that are affected in parallel as defined 
+        ///by the encoder page mappings.
+        public void SetGroupIdxChannels(Dictionary<int, List<int>> value) {
+            channelToGroupIdx.Clear();
+            groupIdxToChannelGroup.Clear();
+            foreach(int groupIdx in value.Keys) {
+                foreach(int channel in value[groupIdx]) {
+                    channelToGroupIdx[channel] = groupIdx;
+                }
+                groupIdxToChannelGroup[groupIdx] = new GroupCounter() { GroupSize = value[groupIdx].Count, SeenSoFar = 0 };
+            }
+        }
+
+        private static Dictionary<int, int> channelToGroupIdx = new Dictionary<int, int>();
+        private static Dictionary<int, GroupCounter> groupIdxToChannelGroup = new Dictionary<int, GroupCounter>();
+
+        ///We need a dictionary that maps from a channel to a 
+        ///Dictionary<GroupIdx, List<Channel>>
+        ///Dictionary<channel, groupIDX>
+        ///Dictionary<groupIDX, channelGroup>
         private Dictionary<int, int> fromValues;
         private Dictionary<int, int> toValues;
         private HashSet<int> channelsSet = new HashSet<int>();
 
-        protected override Dictionary<int, int> GetCurrentValueCore(Dictionary<int, int> defaultOriginValue,
+        public static bool TemporalOffset { get; set; }
+
+        private Dictionary<int, int> temporalOffest(Dictionary<int, int> defaultOriginValue,
                                                                     Dictionary<int, int> defaultDestinationValue,
                                                                     AnimationClock animationClock) {
+            if((From == null || From.Count() == 0) && (To == null || To.Count() == 0)) {
+                return new Dictionary<int, int>();
+            }
+            if(animationClock.CurrentProgress == null) {
+                return null;
+            }
 
-            if(animationClock.CurrentProgress == null){
+            Dictionary<int, int> returnValue = flip ? returnValue1 : returnValue2;
+
+            flip = !flip;
+            returnValue.Clear();
+
+            double progress = animationClock.CurrentProgress.Value;
+            //Choose the correct From/To values
+            fromValues = From ?? defaultOriginValue;
+            toValues = To ?? defaultDestinationValue;
+
+            if(progress == 1.0) {
+                return toValues;
+            }
+
+
+            channelsSet.Clear();
+
+            foreach(int groupIdx in groupIdxToChannelGroup.Keys) {
+                groupIdxToChannelGroup[groupIdx].SeenSoFar = 0;
+            }
+
+            //Interpolate the values
+            foreach(int c in fromValues.Keys) {
+                int newValue = fromValues[c];
+                if(toValues.ContainsKey(c) && channelToGroupIdx.ContainsKey(c) && toValues[c] != newValue) {
+                    //This handles the case of a channel with a toValue and a fromValue
+                    int toValue = toValues[c];
+
+                    int groupIdx = channelToGroupIdx[c];
+                    //UnroundedNewVals is rounded to three decimal places to handle the problem of numerical instability 
+                    //resulting in numbers like .0000001 which gets blown up in the Math.Ceiling call
+                    double unroundedNewVal = Math.Round(((1.0 - progress) * fromValues[c]) + (progress * toValues[c]), 3);
+
+                    double roundedNewVal = Math.Floor(unroundedNewVal);
+
+                    double decimalComponent = unroundedNewVal - roundedNewVal;
+                    double threshold = groupIdxToChannelGroup[groupIdx].SeenSoFar++ / groupIdxToChannelGroup[groupIdx].GroupSize;
+                    if(decimalComponent > threshold) {
+                        newValue = (int)Math.Ceiling(unroundedNewVal);
+                    } else {
+                        newValue = (int)roundedNewVal;
+                    }
+                    if(newValue > 65535) {
+                        newValue = 65535;
+                    }
+                }
+
+                //This handles the case of a channel with a fromValue but not a toValue
+                returnValue[c] = newValue;
+                channelsSet.Add(c);
+            }
+
+            foreach(int c in toValues.Keys) {
+                if(!channelsSet.Contains(c)) {
+                    if(!channelToGroupIdx.ContainsKey(c)) {
+                        continue;
+                    }
+                    //This handles the case of a toValue without a fromValue
+
+                    int toValue = toValues[c];
+                    int groupIdx = channelToGroupIdx[c];
+
+                    double unroundedNewVal = Math.Round(progress * toValues[c], 3);
+
+                    double roundedNewVal = Math.Floor(unroundedNewVal);
+
+                    double decimalComponent = unroundedNewVal - roundedNewVal;
+                    double threshold = groupIdxToChannelGroup[groupIdx].SeenSoFar++ / groupIdxToChannelGroup[groupIdx].GroupSize;
+                    if(decimalComponent > threshold) {
+                        returnValue[c] = (int)Math.Ceiling(unroundedNewVal);
+                    } else {
+                        returnValue[c] = (int)roundedNewVal;
+                    }
+                    if(returnValue[c] > 65535) {
+                        returnValue[c] = 65535;
+                    }
+                }
+                channelsSet.Add(c);
+            }
+
+            return returnValue;
+        }
+
+        private Dictionary<int, int> noTemporalOffest(Dictionary<int, int> defaultOriginValue,
+                                                                    Dictionary<int, int> defaultDestinationValue,
+                                                                    AnimationClock animationClock) {
+            if(animationClock.CurrentProgress == null) {
                 return null;
             }
 
@@ -78,12 +189,37 @@ namespace ThreeByte.DMX
             }
 
             return returnValue;
+
         }
+
+        protected override Dictionary<int, int> GetCurrentValueCore(Dictionary<int, int> defaultOriginValue,
+                                                                    Dictionary<int, int> defaultDestinationValue,
+                                                                    AnimationClock animationClock) {
+
+
+            if(TemporalOffset) {
+                return temporalOffest(defaultOriginValue, defaultDestinationValue, animationClock);
+            } else {
+                return noTemporalOffest(defaultOriginValue, defaultDestinationValue, animationClock);
+            }
+        }
+
+
+
 
         public override bool IsDestinationDefault {
             get {
                 return base.IsDestinationDefault;
             }
         }
+    }
+
+    public class GroupCounter {
+        public GroupCounter() {
+            this.GroupSize = 1.0;
+            this.SeenSoFar = 0;
+        }
+        public double GroupSize { get; set; }
+        public int SeenSoFar { get; set; }
     }
 }
---
+++ Logging/Profiler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ThreeByte.Logging {
+	public class Profiler {
+		public Profiler() {
+			this.startVals = new Dictionary<string, TimeSpan>();
+			this.durations = new Dictionary<string, TimeSpan>();
+			this.longPoll = new Dictionary<string, LongPoll>();
+		}
+		Dictionary<string, TimeSpan> startVals;
+		Dictionary<string, TimeSpan> durations;
+		Dictionary<string, LongPoll> longPoll;
+		public void Start(string name) {
+			this.startVals[name] = DateTime.Now.TimeOfDay;
+		}
+
+		public TimeSpan Stop(string name) {
+			TimeSpan startVal = this.startVals[name];
+			TimeSpan duration = DateTime.Now.TimeOfDay - startVal;
+			this.durations[name] = duration;
+			if(this.longPoll.ContainsKey(name)) {
+				var poll= longPoll[name];
+				poll.Add(duration.Ticks);
+			} else {
+				longPoll[name] = new LongPoll(duration.Ticks);
+			}
+			//Debug.Print(duration.ToString());
+			return duration;
+		}
+
+		public string Dump() {
+			string output = "";
+			foreach(var a in this.durations) {
+				output += string.Format("Name: {0}, Duration: {1}", a.Key, a.Value);
+			}
+			return output;
+		}
+
+		public double Interval(string name) {
+			double duration = 0;
+			if(startVals.ContainsKey(name)) {
+				duration = Stop(name).TotalMilliseconds;
+				duration = TimeSpan.FromTicks(longPoll[name].Average()).TotalMilliseconds;
+			}
+			Start(name);
+			return duration;
+		}
+	}
+
+	public class LongPoll{
+		public LongPoll() {
+			Reset();
+		}
+
+		public void Reset() {
+			this.Count = 0;
+			this.Min = double.MaxValue;
+			this.Max = double.MinValue;
+			this.Sum = 0;
+		}
+
+		public LongPoll(long t) : this() {
+			Add(t);
+		}
+		public void Add(long t) {
+			if(Count % 200 == 0) {
+				Reset();
+			}
+			this.Count++;
+			this.Sum += t;
+			if(t > Max) {
+				Max = t;
+			}else if(t < Min) {
+				Min = t;
+			}
+		}
+
+		public long Average() {
+			return Sum / Count;
+		}
+
+		public double Min { get; set; }
+		public double Max { get; set; }
+		public int Count { get; set; }
+		public long Sum { get; set; }
+
+		public string GetTimeSpanStats() {
+			return string.Format("PROFILER STATS:\nCount: {0}\nMin: {1}ms\nMax: {2}ms\nAverage: {3}ms",
+				Count, TimeSpan.FromTicks((long)Min).TotalMilliseconds, TimeSpan.FromTicks((long)Max).TotalMilliseconds, 
+				TimeSpan.FromTicks((long)Average()).TotalMilliseconds);
+		}
+	}
+}
