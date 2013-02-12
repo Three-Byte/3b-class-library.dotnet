848e521b0d86987381ffe4892533de029cd6148c
Chris Keitel <chris@3-byte.com>
2013-02-12 15:36:01 +0000

    Added control cue animation base.  refs #11498

---
+++ DMX/ControlAnimationBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Animation;
+
+namespace ThreeByte.DMX
+{
+    public abstract class ControlAnimationBase : AnimationTimeline
+    {
+
+        public override Type TargetPropertyType {
+            get { return typeof(bool); }
+        }
+
+        public sealed override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock) {
+            //Validate default origin and destination types
+            return GetCurrentValueCore(defaultOriginValue as bool?, defaultDestinationValue as bool?, animationClock);
+        }
+
+        protected abstract bool GetCurrentValueCore(bool? defaultOriginValue, bool? defaultDestinationValue, AnimationClock animationClock);
+
+    }
+}
---
+++ DMX/LoopPointAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace ThreeByte.DMX {
+    public class LoopPointAnimation : ControlAnimationBase {
+        protected override Freezable CreateInstanceCore() {
+            return new LoopPointAnimation();
+        }
+
+        public static readonly DependencyProperty FromProperty = DependencyProperty.Register("From", typeof(bool), typeof(LoopPointAnimation));
+
+        public bool From {
+            get { return (bool)GetValue(FromProperty); }
+            set { SetValue(FromProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToProperty = DependencyProperty.Register("To", typeof(bool), typeof(LoopPointAnimation));
+
+        public bool To {
+            get { return (bool)GetValue(ToProperty); }
+            set { SetValue(ToProperty, value); }
+        }
+
+        protected override bool GetCurrentValueCore(bool? defaultOriginValue,
+                                                    bool? defaultDestinationValue,
+                                                    AnimationClock animationClock) {
+            return true;
+        }
+
+
+
+
+        public override bool IsDestinationDefault {
+            get {
+                return base.IsDestinationDefault;
+            }
+        }
+    }
+ 
+}
