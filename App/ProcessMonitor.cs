b9c3040178dc2c7c0632197de0fc7e045be9de71
Chris Keitel <chris@3-byte.com>
2013-02-26 15:46:53 +0000

    Test if the process is responding and force reset if it is not.

--- App/ProcessMonitor.cs
+++ App/ProcessMonitor.cs
@@ -43,7 +43,7 @@ namespace ThreeByte.App
 
             //Reasonable defaults
             MaxResourceSnapshots = 1000;
-            ResourceSnapshotInterval = TimeSpan.FromMinutes(5);
+            //ResourceSnapshotInterval = TimeSpan.FromMinutes(5);
             ResourceSnapshotInterval = TimeSpan.FromSeconds(5);
 
             ThreadPool.QueueUserWorkItem(MonitorProcess);
@@ -76,7 +76,11 @@ namespace ThreeByte.App
                             while(!exited) {
                                 //The process is still running.  This is good, just take a snapshot of it
                                 lock(_monitoringProcessLock) {
-                                    LogResourceSnapshot(_monitoringProcess);
+                                    ResourceSnapshot snapshot = LogResourceSnapshot(_monitoringProcess);
+                                    if(snapshot.IsNotResponding) {
+                                        log.WarnFormat("Application is not responding.  Will kill");
+                                        Kill();
+                                    }
                                 }
                                 exited = proc.WaitForExit((int)(ResourceSnapshotInterval.TotalMilliseconds));
                             }
@@ -100,7 +104,7 @@ namespace ThreeByte.App
         }
 
 
-        private void LogResourceSnapshot(Process proc) {
+        private ResourceSnapshot LogResourceSnapshot(Process proc) {
             proc.Refresh();
             ResourceSnapshot snapshot = ResourceSnapshot.FromProcess(proc);
             log.DebugFormat("Process Snapshot: {0}", snapshot);
@@ -111,6 +115,7 @@ namespace ThreeByte.App
                     _resourceSnapshots.Dequeue(); //Just abandon old resource snapshots
                 }
             }
+            return snapshot;
         }
 
         public IEnumerable<ResourceSnapshot> GetSnapshotHistory() {
@@ -163,6 +168,8 @@ namespace ThreeByte.App
         public long PeakWorkingSet { get; private set; }
         public long PrivateMemorySize { get; private set; }
         public int ThreadCount { get; private set; }
+        public int HandleCount { get; private set; }
+        public bool IsNotResponding { get; private set; }
 
         private ResourceSnapshot() {
             Timestamp = DateTime.Now;
@@ -173,15 +180,16 @@ namespace ThreeByte.App
                 PeakPagedMemorySize = process.PeakPagedMemorySize64,
                 PeakWorkingSet = process.PeakWorkingSet64,
                 PrivateMemorySize = process.PrivateMemorySize64,
-                ThreadCount = process.Threads.Count
-
+                ThreadCount = process.Threads.Count,
+                HandleCount = process.HandleCount,
+                IsNotResponding = !process.Responding
             };
             return newSnapshot;
         }
 
         public override string ToString() {
-            return string.Format("{0} - PeakPaged: {1} PeakWorking: {2} PrivateMemory: {3} ThreadCount: {4}",
-                Timestamp, PeakPagedMemorySize, PeakWorkingSet, PrivateMemorySize, ThreadCount);
+            return string.Format("{0} - PeakPaged: {1} PeakWorking: {2} PrivateMemory: {3} ThreadCount: {4} HandleCount: {5} IsNotResponding: {6}",
+                Timestamp, PeakPagedMemorySize, PeakWorkingSet, PrivateMemorySize, ThreadCount, HandleCount, IsNotResponding);
         }
     }
 }
