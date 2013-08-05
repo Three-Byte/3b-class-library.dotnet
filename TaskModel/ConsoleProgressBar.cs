3e6b088edfe7705b2657b37a465151eb3696d9c8
Chris Keitel <chris@3-byte.com>
2013-08-05 15:58:52 +0000

    More feedback on the consoleprogressbar.

--- TaskModel/ConsoleProgressBar.cs
+++ TaskModel/ConsoleProgressBar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -11,8 +12,26 @@ namespace ThreeByte.TaskModel
         public int Total { get; set; }
         public int Current { get; private set; }
         public double Percent { get; private set; }
+        public TimeSpan Elapsed { get { return _stopwatch.Elapsed; } }
+        public double TicksPerSecond {
+            get {
+                lock(TickQueue) {
+                    if(TickQueue.Count < 2) {
+                        return 0.0;
+                    }
+                    TickCount start = TickQueue.First();
+                    TickCount end = TickQueue.Last();
+                    return ((end.Ticks - start.Ticks) / (end.Timestamp - start.Timestamp).TotalSeconds);
+                }
+            }
+        }
+
+
+        private Queue<TickCount> TickQueue = new Queue<TickCount>();
+
 
         private bool _isComplete = false;
+        private Stopwatch _stopwatch;
 
         public void Init(int total) {
             Total = total;
@@ -20,6 +39,10 @@ namespace ThreeByte.TaskModel
             Percent = 0.0;
             Console.CursorVisible = false;
             _isComplete = false;
+            _stopwatch = Stopwatch.StartNew();
+            lock(TickQueue) {
+                TickQueue.Clear();
+            }
             Draw();
         }
 
@@ -38,7 +61,17 @@ namespace ThreeByte.TaskModel
                 Complete();
                 return;
             }
+            UpdateTickQueue(Current);
             Draw();
+        }
+
+        private void UpdateTickQueue(int ticks) {
+            lock(TickQueue) {
+                TickQueue.Enqueue(new TickCount(ticks));
+                while(TickQueue.Count > 5) {
+                    TickQueue.Dequeue();
+                }
+            }
         }
 
         public void Complete() {
@@ -54,18 +87,30 @@ namespace ThreeByte.TaskModel
                 throw new InvalidOperationException("Cannot update after progress is complete");
             }
 
-            string output = string.Empty;
             Percent = System.Math.Round((Current * 100.0) / (double)Total, 2);
 
-            output = "\r[";
+            StringBuilder output = new StringBuilder();
+            output.Append("\r[");
             int stepsToDraw = (int)(Percent / 2);
             for(int i = 0; i < 50; i++) {
-                output += (i < stepsToDraw ? "=" : (i == stepsToDraw ? ">" : " "));
+                output.Append((i < stepsToDraw ? "=" : (i == stepsToDraw ? ">" : " ")));
             }
-            output += string.Format("]\t{0:0.00}%\t", Percent);
-            Console.Write(output);
+            output.AppendFormat("]\t{0:0.00}%\t{1:hh\\:mm\\:ss} {2:0.0}", Percent, Elapsed, TicksPerSecond);
+            Console.Write(output.ToString());
         }
 
+        private class TickCount
+        {
+            public int Ticks { get; private set; }
+            public DateTime Timestamp { get; private set; }
+
+            public TickCount(int ticks) {
+                Ticks = ticks;
+                Timestamp = DateTime.Now;
+            }
+        }
 
     }
+
+    
 }
