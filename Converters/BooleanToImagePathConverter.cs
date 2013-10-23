f40d239cf3cebb5991c9291e9b0782b0b2b1c146
Mike <mike@3-byte.com>
2013-10-23 21:51:39 +0000

    Added BooleanToImagePathConverter.cs

---
+++ Converters/BooleanToImagePathConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+
+//author: mike@3-byte.com
+
+namespace ThreeByte.Converters
+{
+    public class BooleanToStringConverter : IValueConverter
+    {
+        public string TruePath { get; set; }
+        public string FalsePath { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            bool b = (bool)value;
+
+            if (b) {
+                return TruePath;
+            } else {
+                return FalsePath;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
