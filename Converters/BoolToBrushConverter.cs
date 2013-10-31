3c11d24942a0544343060a41ff424a957b24bf78
Mike <mike@3-byte.com>
2013-10-31 16:38:10 +0000

    Added BoolToBrushConverter.cs

---
+++ Converters/BoolToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ThreeByte.Converters {
+    public class BoolToBrushConverter : IValueConverter {
+
+        public SolidColorBrush TrueBrush { get; set; }
+        public SolidColorBrush FalseBrush { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            bool val = bool.Parse(value.ToString());
+
+            return val ? TrueBrush : FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
