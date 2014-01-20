707e6ea4551ce8505621167e181e12b61e12fb75
Amichai Levy <amichai@3-byte.com>
2014-01-20 22:44:59 +0000

    Further implemented the AjaClip class and implemented deserializaiton of the *javascript* returned by aja webservice API. refs #16069 @2h

--- Network/Devices/AjaClip.cs
+++ Network/Devices/AjaClip.cs
@@ -5,35 +5,90 @@ using System.Web;
 
 namespace ThreeByte.Network.Devices {
     public class AjaClip {
-        public AjaClip(string name) {
-            this.Name = name;
-            this.Timecode = "00:00:04";
+        public string Name {
+            get {
+                return this.clipname.Split('.').First();
+            }
         }
-        public string Name { get; set; }
-        public string Timecode { get; set; }
+
+        public string Timecode {
+            get {
+                return "timecode...";
+            }
+        }
+        public string clipname { get; set; }
+        public string timestamp { get; set; }
+        public string fourcc { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+        public string framecount { get; set; }
+        public string framerate { get; set; }
+        public string interlace { get; set; }
 
         internal static List<AjaClip> Load(string content) {
 
             ////TODO: user Iron JS to parse this string into an object 
             ///and query the individual parameters
-
-            var result = content.TrimEnd(';');
-            var tokens = result.Split(' ').ToList();
-            result = string.Concat(tokens.Select(i => {
-                var split = i.Split(':');
-                if (i.TakeWhile(c => c == ':').Count() == 1) {
-                    return "\"" + split[0] + "\"" + ":" + split[1];
-                } else {
-                    return i + " ";
+            List<AjaClip> toReturn = new List<AjaClip>();
+            string currentKey = "";
+            AjaClip inspectionClip = new AjaClip();
+            var tokens = content.Split(' ');
+            for (int i = 0; i < tokens.Count(); i++) {
+                var token = tokens[i];
+                if (token == "[" || token == "]") {
+                    continue;
                 }
-            }));
+                if (token.Contains("}")) {
+                    toReturn.Add(inspectionClip);
+                    inspectionClip = new AjaClip();
+                    continue;
+                }
 
-            //var content1 = IronJS.Hosting.FSharp.createContext();
-            //IronJS.Hosting.CSharp.Context ctx = new IronJS.Hosting.CSharp.Context();
-            //ctx.Execute("var a= " + content);
-            //var result2 = ctx.Execute("a");
+                if (char.IsLetter(token.First())) {
+                    currentKey = token.Substring(0, token.Length - 1);
+                    continue;
+                }
+                if (token.First() == '"') {
+                    var val = token.TrimEnd(',').Trim('"');
 
-            throw new NotImplementedException();
+                    while(string.Concat(token.Skip(token.Length - 2)) != "\"," && token.Last() != '"') {
+                        token = tokens[++i];
+                        val += " " + token;
+                        
+                    }
+                    ///Value
+                    switch (currentKey) {
+                        case "clipname":
+                            inspectionClip.clipname = val;
+                            break;
+                        case "timestamp":
+                            inspectionClip.timestamp = val;
+                            break;
+                        case "fourcc":
+                            inspectionClip.fourcc = val;
+                            break;
+                        case "width":
+                            inspectionClip.width = val;
+                            break;
+                        case "height":
+                            inspectionClip.height = val;
+                            break;
+                        case "framecount":
+                            inspectionClip.framecount = val;
+                            break;
+                        case "framerate":
+                            inspectionClip.framerate = val;
+                            break;
+                        case "interlace":
+                            inspectionClip.interlace = val;
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                    continue;
+                }
+            }
+            return toReturn;
         }
     }
 }
